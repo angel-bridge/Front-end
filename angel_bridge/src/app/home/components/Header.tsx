ea4ba08a85e2b6cad6e260970610e391227f0045
Gabin Lee <gb94@ewhain.net>
2024-12-16 19:51:47 +0900

    [ design ] 홈 화면 헤더 퍼블리싱

---
+++ angel_bridge/src/app/home/components/Header.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Logo from '@/app/home/assets/Logo_purple.svg';
+import HomeIconColor from '@/app/home/assets/HomeIcon_color.svg';
+import ProgramIconColor from '@/app/home/assets/ProgramIcon_color.svg';
+import QnAIconColor from '@/app/home/assets/QnAIcon_color.svg';
+import HomeIconGrey from '@/app/home/assets/HomeIcon_grey.svg';
+import ProgramIconGrey from '@/app/home/assets/ProgramIcon_grey.svg';
+import QnAIconGrey from '@/app/home/assets/QnAIcon_grey.svg';
+
+import * as styles from '@/app/home/styles/Header.css';
+
+export default function Header() {
+    const [activeTab, setActiveTab] = useState("home");
+
+    const tabs = [
+        { id: "home", label: "홈", colorIcon: HomeIconColor, greyIcon: HomeIconGrey },
+        { id: "program", label: "프로그램", colorIcon: ProgramIconColor, greyIcon: ProgramIconGrey },
+        { id: "qna", label: "문의", colorIcon: QnAIconColor, greyIcon: QnAIconGrey },
+    ];
+
+    return (
+        <div className={styles.header}>
+            <Image src={Logo} alt="MainLogo"/>
+            <div className={styles.menuTabWrapper}>
+                {tabs.map((tab) => (
+                    <div
+                        key={tab.id}
+                        className={activeTab === tab.id ? styles.colorMenuTab : styles.greyMenuTab}
+                        onClick={() => setActiveTab(tab.id)}
+                    >
+                        <Image
+                            src={activeTab === tab.id ? tab.colorIcon : tab.greyIcon}
+                            alt={`${tab.label}Button`}
+                        />
+                        <div className={activeTab === tab.id ? styles.colorTabText : styles.greyTabText}>
+                            {tab.label}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.loginButton}>
+                <p className={styles.buttonText}>로그인</p>
+            </div>
+        </div>
+    )
+}
---
+++ angel_bridge/src/app/home/styles/Header.css.ts
@@ -0,0 +1,87 @@
+import { style } from '@vanilla-extract/css'
+
+export const header = style({
+    display: 'flex',
+    width: '100vw',
+    padding: '1rem 2.25rem',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    position: 'fixed',
+    top: '0px',
+    zIndex: 10,
+});
+
+export const loginButton = style({
+    width: '7.5rem',
+    height: '2.5rem',
+    padding: '0.5rem 1.5rem',
+    justifyItems: 'center',
+    alignItems: 'center',
+    borderRadius: '1.875rem',
+    background: '#21272A',
+    cursor: 'pointer',
+});
+
+export const buttonText = style({
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: '1rem',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: '150%',
+    letterSpacing: '-0.02rem',
+});
+
+export const menuTabWrapper = style({
+    display: 'flex',
+    height: '3rem',
+    padding: '0.375rem 0.5rem',
+    alignItems: 'center',
+    borderRadius: '1.875rem',
+    background: '#F2F4F8',
+});
+
+export const colorMenuTab = style({
+    display: 'flex',
+    width: '7.5rem',
+    height: '2.25rem',
+    padding: '0.25rem 0.75rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '0.25rem',
+    borderRadius: '1.875rem',
+    background: '#6C54F6',
+    cursor: 'pointer',
+});
+
+export const greyMenuTab = style({
+    display: 'flex',
+    width: '7.5rem',
+    height: '2.25rem',
+    padding: '0.25rem 0.75rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '0.25rem',
+    background: 'transparent',
+    cursor: 'pointer',
+});
+
+export const colorTabText = style({
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: '1.125rem',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: '150%',
+    letterSpacing: '-0.0225rem',
+});
+
+export const greyTabText = style({
+    color: '#697077',
+    textAlign: 'center',
+    fontSize: '1.125rem',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: '150%',
+    letterSpacing: '-0.0225rem',
+});
