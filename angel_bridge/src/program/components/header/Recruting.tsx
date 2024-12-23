16546241663de799b46395b37232c4fded7de271
HAE2NI <hae0_02ni@ewhain.net>
2024-12-23 10:43:35 +0900

    design: program

---
+++ angel_bridge/src/program/components/header/Recruting.tsx
@@ -0,0 +1,3 @@
+export default function Recruting() {
+  return <div>Recruting</div>
+}
---
+++ angel_bridge/src/program/styles/recruteBtn.css.ts
@@ -0,0 +1,6 @@
+import {style} from '@vanilla-extract/css';
+
+export const recruteBtn = style({
+    display: "flex",
+    justifyContent:
+})
