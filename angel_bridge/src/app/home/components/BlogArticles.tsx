f4d770393b6e9ccd268621db412c388fe621afc5
Gabin Lee <gb94@ewhain.net>
2024-12-30 13:15:15 +0900

    [ refactor ] 블로그 아티클 콘텐츠 컴포넌트 map 방식으로 수정

--- angel_bridge/src/app/home/components/BlogArticles.tsx
+++ angel_bridge/src/app/home/components/BlogArticles.tsx
@@ -3,6 +3,21 @@ import * as style from '@/app/home/styles/BlogArticles.css';
 import Image from 'next/image';
 import ProfileImage from '@/app/home/assets/ProfileImage.jpg';
 
+const contentsDummyData = [
+    {
+        title: "[안 읽으면 손해!] 2025 스타트업 지원 사업 총정리",
+        text: "블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요 블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요",
+        writer: "엔젤브릿지",
+        date: "2024. 12. 16",
+    },
+    {
+        title: "[안 읽으면 손해!] 2025 스타트업 지원 사업 총정리",
+        text: "블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요 블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요",
+        writer: "엔젤브릿지",
+        date: "2024. 12. 16",
+    },
+];
+
 export default function BlogArticles() {
     return (
         <div className={style.articles}>
@@ -11,54 +26,28 @@ export default function BlogArticles() {
                 <p className={style.seeAll}>전체보기</p>
             </div>
             <div className={style.contentsWrapper}>
-                <div className={style.contents}>
-                    <div className={style.textWrapper}>
-                        <p className={style.contentTitle}>
-                        [안 읽으면 손해!] 2025 스타트업 지원 사업 총정리
-                        </p>
-                        <p className={style.contentText}>
-                        블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요 블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요 
-                        </p>
-                    </div>
-                    <div className={style.writerWrapper}>
-                        <Image
-                            src={ProfileImage}
-                            alt="ProfileImage"
-                            width={24}
-                            height={24}
-                            style={{
-                                objectFit: 'cover',
-                                borderRadius: '1.5rem',
-                            }}
+            {contentsDummyData.map((content, index) => (
+                    <div key={index} className={style.contents}>
+                        <div className={style.textWrapper}>
+                            <p className={style.contentTitle}>{content.title}</p>
+                            <p className={style.contentText}>{content.text}</p>
+                        </div>
+                        <div className={style.writerWrapper}>
+                            <Image
+                                src={ProfileImage}
+                                alt="ProfileImage"
+                                width={24}
+                                height={24}
+                                style={{
+                                    objectFit: 'cover',
+                                    borderRadius: '1.5rem',
+                                }}
                             />
-                        <p className={style.writer}>엔젤브릿지</p>
-                        <p className={style.writtenDate}>2024. 12. 16</p>
+                            <p className={style.writer}>{content.writer}</p>
+                            <p className={style.writtenDate}>{content.date}</p>
+                        </div>
                     </div>
-                </div>
-                <div className={style.contents}>
-                    <div className={style.textWrapper}>
-                        <p className={style.contentTitle}>
-                        [안 읽으면 손해!] 2025 스타트업 지원 사업 총정리
-                        </p>
-                        <p className={style.contentText}>
-                        블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요 블로그 첫 시작 문구를 가져와 주세요 두줄정도가 적당할 것 같다고 생각 중이에요 
-                        </p>
-                    </div>
-                    <div className={style.writerWrapper}>
-                        <Image
-                            src={ProfileImage}
-                            alt="ProfileImage"
-                            width={24}
-                            height={24}
-                            style={{
-                                objectFit: 'cover',
-                                borderRadius: '1.5rem',
-                            }}
-                            />
-                        <p className={style.writer}>엔젤브릿지</p>
-                        <p className={style.writtenDate}>2024. 12. 16</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
