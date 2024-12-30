dc214ecfd00a4e1e35d1bf10cbdc205dfa583cc9
HAE2NI <hae0_02ni@ewhain.net>
2024-12-30 11:56:01 +0900

    design: mypage 회원정보

--- angel_bridge/src/mypage/memberInfo/components/Info.tsx
+++ angel_bridge/src/mypage/memberInfo/components/Info.tsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import Button from './Button'
-import InputTag from './InputTag'
-import { INFO_LIST } from '../core/info'
+import { INFO_LIST, MYPAGE_BTN_LIST } from '../core/info'
 import InputWithTag from './InputWithTag'
-
 
 export default function Info() {
   return (
-<div>
-<div>
-    <Button />
-    <Button />
-
-</div>
-<div>
-
-</div>
-{INFO_LIST.map((info) => {
-    return (
-        <InputWithTag  key={info} info={info} />
-    )
-})}
-</div>
-
-)
+    <div>
+      <div>
+        {MYPAGE_BTN_LIST.map((text) => {
+          return <Button text={text} key={text} />
+        })}
+      </div>
+      <div>
+        {INFO_LIST.map((data) => {
+          const { info, infodetail } = data
+          return <InputWithTag info={info} infodetail={infodetail} key={info} />
+        })}
+      </div>
+    </div>
+  )
 }
--- angel_bridge/src/mypage/memberInfo/core/info.ts
+++ angel_bridge/src/mypage/memberInfo/core/info.ts
@@ -1,1 +1,7 @@
-export const INFO_LIST = ['닉네임', '전화번호', '이메일 주소']
+export const INFO_LIST = [
+  { info: '닉네임', infodetail: '현종혁' },
+  { info: '전화번호호', infodetail: '010-2711-2771' },
+  { info: '이메일주소', infodetail: 'ZongheyokZZangZZangman@gmail.com' },
+]
+
+export const MYPAGE_BTN_LIST = ['회원정보', '결제내역']
