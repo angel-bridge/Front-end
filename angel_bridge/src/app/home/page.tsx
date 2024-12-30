958b5ef7e109dee72d1738c3faa50179b22b1022
Gabin Lee <gb94@ewhain.net>
2024-12-30 15:26:28 +0900

    [ design ] 홈 화면 헤더 theme 스타일 깨짐 수정

--- angel_bridge/src/app/home/page.tsx
+++ angel_bridge/src/app/home/page.tsx
@@ -1,15 +1,16 @@
-import Header from './components/Header'
-import Banner from './components/Banner'
-import Contents from './components/Contents'
-import Footer from './components/Footer'
+import { grayScale, purple } from '@/style/theme.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Contents from './components/Contents';
+import Footer from './components/Footer';
 
 export default function Home() {
     return (
-        <div>
+        <div className={[grayScale, purple].join(' ')}>
             <Header />
             <Banner />
             <Contents />
             <Footer />
         </div>
-    )
+    );
 }
