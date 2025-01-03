054dfd807e6494b64685601319b9b1c3f986d8fe
HAE2NI <hae0_02ni@ewhain.net>
2025-01-03 18:59:40 +0900

    [ feat ] email error

--- angel_bridge/src/mypage/memberInfo/components/EmailInput.tsx
+++ angel_bridge/src/mypage/memberInfo/components/EmailInput.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { input, input_tag, inputwithtag_container } from '../styles/input.css'
+import ErrorMessage from './ErrorMessage'
 
 export default function EmailInput() {
+  const [value, setValue] = useState('')
+  const [isError, setIsError] = useState(false)
+
+  function handleValueError() {
+    const email = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/
+
+    if (email.test(value) === false) {
+      setIsError(true)
+    } else {
+      setIsError(false)
+    }
+  }
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value)
+    handleValueError()
+  }
+
   return (
     <div className={inputwithtag_container}>
       <p className={input_tag}>이메일 주소 *</p>
-      <input placeholder="sadfdsfsdf@adsfsd.com" className={input} />
+      <input
+        onChange={handleInputChange}
+        placeholder="sadfdsfsdf@adsfsd.com"
+        className={input}
+      />
+      {isError && <ErrorMessage number={2} />}
     </div>
   )
 }
