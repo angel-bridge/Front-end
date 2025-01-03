eb3ad57c0ae4f4d14340b1a827c551e051db7abe
HAE2NI <hae0_02ni@ewhain.net>
2025-01-03 22:28:31 +0900

    [ feat ] phone num error case 관리리

--- angel_bridge/src/mypage/memberInfo/components/PhoneNumberInput.tsx
+++ angel_bridge/src/mypage/memberInfo/components/PhoneNumberInput.tsx
@@ -1,23 +1,21 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { input, input_tag, inputwithtag_container } from '../styles/input.css'
 import ErrorMessage from './ErrorMessage'
 import { InputPropTypes } from '../types/inputProps'
 
 export default function PhoneNumberInput(props: InputPropTypes) {
-  const { value, onChange } = props
-  const [isError, setIsError] = useState(false)
+  const { value, onChange, isError, setIsError } = props
   const pattern = /^(010|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/
 
   useEffect(() => {
-    console.log(value)
-    console.log(isError)
-    if (pattern.test(value) === false) {
+    if (value && pattern.test(value) === false) {
       setIsError(true)
     } else {
       setIsError(false)
     }
-  }, [value, isError])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value])
 
   return (
     <div className={inputwithtag_container}>
