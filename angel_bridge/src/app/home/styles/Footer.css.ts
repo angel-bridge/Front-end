a91d79449eb5b8a6dc095ada3c1640eabc94db40
Gabin Lee <gb94@ewhain.net>
2025-01-05 15:05:23 +0900

    [ fix ] footer 스타일 코드 수정

--- angel_bridge/src/app/home/styles/Footer.css.ts
+++ angel_bridge/src/app/home/styles/Footer.css.ts
@@ -1,10 +1,11 @@
 import { style } from '@vanilla-extract/css'
 import { vars } from '@/style/theme.css';
+import * as font from '@/style/theme.css'
 
 export const footer = style({
     display: 'flex',
     width: '100vw',
-    padding: '1.25rem 12.5rem',
+    padding: '2rem 20rem',
     boxSizing: 'border-box',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
@@ -14,21 +15,19 @@ export const footer = style({
 
 export const logoWrapper = style({
     display: 'flex',
-    gap: '0.5rem',
+    gap: '0.8rem',
 });
 
 export const buttonWrapper = style({
     display: 'flex',
     alignItems: 'flex-start',
-    gap: '1.875rem',
+    gap: '2.9rem',
 });
 
-export const button = style({
-    color: vars.grayScale.grayscale300,
-    fontSize: '1rem',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    lineHeight: '150%',
-    letterSpacing: '-0.02rem',
-    cursor: 'pointer',
-});
+export const button = style([
+    font.title16SB,
+    {
+        color: vars.grayScale.grayscale300,
+        cursor: 'pointer',
+    }
+]);
