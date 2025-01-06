ede3bd1af6fdfee3ba4031820ff69ee5de52f6b1
HAE2NI <hae0_02ni@ewhain.net>
2025-01-06 15:05:08 +0900

    [ design ] page component

---
+++ angel_bridge/src/inquiry/components/PageComponent.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { INQUIRY_LIST } from '../core/inquiry'
+import Container from './Container'
+import { pagecompo_style } from '../styles/container.css'
+
+export default function PageComponent() {
+  return (
+    <div className={pagecompo_style}>
+      {INQUIRY_LIST.map((data) => {
+        const { title, discription, image } = data
+        return (
+          <Container
+            key={title}
+            title={title}
+            discription={discription}
+            image={image}
+          />
+        )
+      })}
+    </div>
+  )
+}
---
+++ angel_bridge/src/inquiry/core/inquiry.ts
@@ -0,0 +1,15 @@
+import FAQ from '../assets/FQA.png'
+import ASK from '../assets/ASK.png'
+
+export const INQUIRY_LIST = [
+  {
+    title: '자주묻는 질문',
+    discription: 'FAQ 확인하기',
+    image: FAQ,
+  },
+  {
+    title: '문의하기',
+    discription: '오픈채팅방 바로가기',
+    image: ASK,
+  },
+]
