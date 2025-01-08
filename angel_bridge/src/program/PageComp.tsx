ce51f0c8c5217dd5e553472aca869a37c339e604
HAE2NI <hae0_02ni@ewhain.net>
2025-01-08 13:50:57 +0900

    [ etc ] delete codes

--- angel_bridge/src/program/PageComp.tsx
+++ angel_bridge/src/program/PageComp.tsx
@@ -1,28 +1,12 @@
-'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import ProgramHeader from './components/header/ProgramHeader'
 import MainProgram from './components/main/MainProgram'
 import { page_compo_container } from './styles/pageCompo.css'
 
 export default function PageComp() {
-  const [isClickUpcoming, setIsClickUpcoming] = useState(false)
-  const [isClickOngoing, setIsClickOngoing] = useState(false)
-
-  function handleClickUpcoming() {
-    setIsClickUpcoming(!isClickUpcoming)
-  }
-
-  function handleClickOngoing() {
-    setIsClickOngoing(!isClickOngoing)
-  }
   return (
     <div className={page_compo_container}>
-      <ProgramHeader
-        isClickOngoing={isClickOngoing}
-        isClickUpcoming={isClickUpcoming}
-        handleClickUpcoming={handleClickUpcoming}
-        handleClickOngoing={handleClickOngoing}
-      />
+      <ProgramHeader />
       <MainProgram />
     </div>
   )
