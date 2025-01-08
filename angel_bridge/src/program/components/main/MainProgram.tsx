4b345f79c728e6a7888dd9dcc2eb32aa6dadd783
HAE2NI <hae0_02ni@ewhain.net>
2025-01-08 13:51:14 +0900

    [ feat ] add data useEffect

--- angel_bridge/src/program/components/main/MainProgram.tsx
+++ angel_bridge/src/program/components/main/MainProgram.tsx
@@ -1,15 +1,36 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Programs from './Programs'
 import { length_style, main_container } from './styles/programmain.css'
+import { useProgramStore } from '@/program/store/useProgramStore'
 import useGetAllPrograms from '@/program/api/hooks/useGetAllPrograms'
+import useGetOngoings from '@/program/api/hooks/useGetOngoings'
+import useGetUpcoming from '@/program/api/hooks/useGetUpcoming'
 
 export default function MainProgram() {
-  const { data } = useGetAllPrograms()
+  const { program, setProgram, isClickOngoings, isClickUpcomings } =
+    useProgramStore()
+  const { data: all, isLoading } = useGetAllPrograms()
+  const { data: ongoings } = useGetOngoings()
+  const { data: upcomings } = useGetUpcoming()
+
+  useEffect(() => {
+    if (isClickOngoings && ongoings) {
+      setProgram(ongoings)
+    } else if (isClickUpcomings && upcomings) {
+      setProgram(upcomings)
+    } else if (all) {
+      setProgram(all)
+    }
+  }, [all, ongoings, upcomings, program, isClickOngoings, isClickUpcomings])
+
   return (
-    <div className={main_container}>
-      <p className={length_style}>전체 {data?.length}개</p>
-      <Programs programs={data} />
-    </div>
+    <>
+      {isLoading && <p>loading...</p>}
+      <div className={main_container}>
+        <p className={length_style}>전체 {program?.length}개</p>
+        <Programs programs={program} />
+      </div>
+    </>
   )
 }
