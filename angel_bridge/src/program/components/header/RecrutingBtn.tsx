f4bf47f4496befef90985680c7d5dc935d675ae5
HAE2NI <hae0_02ni@ewhain.net>
2025-01-08 13:50:49 +0900

    [ feat ] add buttonhandler

--- angel_bridge/src/program/components/header/RecrutingBtn.tsx
+++ angel_bridge/src/program/components/header/RecrutingBtn.tsx
@@ -1,16 +1,18 @@
+'use client'
+import { useProgramStore } from '@/program/store/useProgramStore'
 import { blackbutton, graybutton } from '@/program/styles/button.css'
 
-export default function RecrutingBtn({
-  handleClickOngoing,
-  isClickOngoing,
-}: {
-  handleClickOngoing: () => void
-  isClickOngoing: boolean
-}) {
+export default function RecrutingBtn() {
+  const { isClickOngoings, setIsClickOngoings } = useProgramStore()
+
+  function handleOnClick() {
+    setIsClickOngoings()
+  }
+
   return (
     <button
-      onClick={handleClickOngoing}
-      className={isClickOngoing ? blackbutton : graybutton}
+      onClick={handleOnClick}
+      className={isClickOngoings ? blackbutton : graybutton}
     >
       모집중
     </button>
--- angel_bridge/src/program/components/header/WillRecruteBtn.tsx.tsx
+++ angel_bridge/src/program/components/header/WillRecruteBtn.tsx.tsx
@@ -1,16 +1,17 @@
+'use client'
+import { useProgramStore } from '@/program/store/useProgramStore'
 import { blackbutton, graybutton } from '@/program/styles/button.css'
 
-export default function WillRecruteBtn({
-  handleClickUpcoming,
-  isClickUpcoming,
-}: {
-  handleClickUpcoming: () => void
-  isClickUpcoming: boolean
-}) {
+export default function WillRecruteBtn() {
+  const { isClickUpcomings, setIsClickUpcoming } = useProgramStore()
+
+  function handleOnClick() {
+    setIsClickUpcoming() // 상태를 토글
+  }
   return (
     <button
-      onClick={handleClickUpcoming}
-      className={isClickUpcoming ? blackbutton : graybutton}
+      onClick={handleOnClick}
+      className={isClickUpcomings ? blackbutton : graybutton}
     >
       모집 예정
     </button>
