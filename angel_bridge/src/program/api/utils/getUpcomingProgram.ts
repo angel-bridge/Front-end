a3f683919a4371d6d6f033b616bbd488024aee11
HAE2NI <hae0_02ni@ewhain.net>
2025-01-08 09:51:52 +0900

    [ feat ] getUpcoming

---
+++ angel_bridge/src/program/api/utils/getUpcomingProgram.ts
@@ -0,0 +1,23 @@
+import { instance } from '@/api/instance'
+
+export interface ResultType {
+  educationId: number
+  preImage: string
+  description: string
+  title: string
+  recruitmentStatus: string
+}
+
+interface AllProgramType {
+  timestamp: string
+  code: number
+  message: string
+  result: ResultType[]
+}
+
+export const getUpcomingProgram = async () => {
+  const response: AllProgramType = await instance.get(
+    `/api/v1/education/upcoming`,
+  )
+  return response.result
+}
