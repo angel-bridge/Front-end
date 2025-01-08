3e86a3e70f050b5d3fe8ecd6bf3dc210be4499f4
HAE2NI <hae0_02ni@ewhain.net>
2025-01-08 13:50:37 +0900

    [ etc ] delete codes

--- angel_bridge/src/program/components/header/ProgramHeader.tsx
+++ angel_bridge/src/program/components/header/ProgramHeader.tsx
@@ -5,31 +5,12 @@ import Search from './Search'
 import { button_container } from '@/program/styles/button.css'
 import { header_style } from '@/program/styles/header.css'
 
-interface ProgramHeaderProps {
-  handleClickOngoing: () => void
-  handleClickUpcoming: () => void
-  isClickOngoing: boolean
-  isClickUpcoming: boolean
-}
-
-export default function ProgramHeader(props: ProgramHeaderProps) {
-  const {
-    handleClickOngoing,
-    handleClickUpcoming,
-    isClickOngoing,
-    isClickUpcoming,
-  } = props
+export default function ProgramHeader() {
   return (
     <div className={header_style}>
       <div className={button_container}>
-        <RecrutingBtn
-          isClickOngoing={isClickOngoing}
-          handleClickOngoing={handleClickOngoing}
-        />
-        <WillRecruteBtn
-          isClickUpcoming={isClickUpcoming}
-          handleClickUpcoming={handleClickUpcoming}
-        />
+        <RecrutingBtn />
+        <WillRecruteBtn />
       </div>
 
       <Search />
