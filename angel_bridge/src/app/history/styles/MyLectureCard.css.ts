5cd342834dd746bed4be210e25279560900a90f9
Gabin Lee <gb94@ewhain.net>
2025-01-08 12:16:02 +0900

    [ fix ] 수강내역 컴포넌트 스타일 코드 수정

--- angel_bridge/src/app/history/styles/MyLectureCard.css.ts
+++ angel_bridge/src/app/history/styles/MyLectureCard.css.ts
@@ -1,13 +1,14 @@
 import { style } from '@vanilla-extract/css'
-import { vars } from '@/style/theme.css';
+import { vars } from '@/style/theme.css'
+import * as font from '@/style/theme.css'
 
 export const contents = style({
     display: 'flex',
-    width: '31.25rem',
+    width: '50rem',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    padding: '1.75rem',
-    gap: '1.5rem',
+    padding: '2.8rem',
+    gap: '2.4rem',
     borderRadius: '24px',
     background: vars.purple.purple100,
     transition: 'transform 0.7s ease',
@@ -18,8 +19,8 @@ export const contents = style({
 });
 
 export const contentImage = style({
-    width: '11.25rem',
-    height: '11.25rem',
+    width: '18rem',
+    height: '18rem',
     borderRadius: '24px',
     overflow: 'hidden',
     // position: 'relative',
@@ -28,7 +29,7 @@ export const contentImage = style({
 export const contentInfo= style({
     display: 'flex',
     alignItems: 'flex-start',
-    gap: '1.625rem',
+    gap: '2.6rem',
     alignSelf: 'stretch',
 });
 
@@ -36,36 +37,30 @@ export const contentText = style({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    gap: '0.75rem',
+    gap: '1.2rem',
     flex: '1 0 0',
 });
 
-export const contentPeriod = style({
-    width: '100%',
-    color: vars.grayScale.grayscale300,
-    fontSize: '1rem',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    lineHeight: '150%',
-    letterSpacing: '-0.02rem',
-});
+export const contentPeriod = style([
+    font.body16M,
+    {
+        width: '100%',
+        color: vars.grayScale.grayscale300,
+    }
+]);
 
-export const contentTitle = style({
-    width: '100%',
-    color: vars.grayScale.grayscale800,
-    fontSize: '1.5rem',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    lineHeight: '150%',
-    letterSpacing: '-0.03rem',
-});
+export const contentTitle = style([
+    font.title24SB,
+    {
+        width: '100%',
+        color: vars.grayScale.grayscale800,
+    }
+]);
 
-export const contentBio = style({
-    width: '100%',
-    color: vars.grayScale.grayscale600,
-    fontSize: '1.125rem',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    lineHeight: '150%',
-    letterSpacing: '-0.0225rem',
-});
+export const contentBio = style([
+    font.body18M,
+    {
+        width: '100%',
+        color: vars.grayScale.grayscale600,
+    }
+]);
--- angel_bridge/src/components/common/Button.css.ts
+++ angel_bridge/src/components/common/Button.css.ts
@@ -1,26 +1,26 @@
 import { style, styleVariants } from '@vanilla-extract/css'
-import { vars } from '@/style/theme.css';
+import { vars } from '@/style/theme.css'
+import * as font from '@/style/theme.css'
 
 export const signupButton = style({
     display: 'flex',
-    width: '30rem',
-    height: '4rem',
-    padding: '1rem, 2.5rem',
+    width: '44.4rem',
+    height: '6.4rem',
+    padding: '1.6rem 2.0rem',
     justifyContent: 'center',
     alignItems: 'center',
-    gap: '0.75rem',
+    gap: '1.2rem',
     borderRadius: '70px',
+    border: 'none',
     cursor: 'pointer',
 });
 
-export const signupText = style({
-    color: vars.grayScale.grayscale00,
-    fontSize: '1.25rem',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    lineHeight: '150%',
-    letterSpacing: '-0.025rem',
-});
+export const signupText = style([
+    font.title20SB,
+    {
+        color: vars.grayScale.grayscale00,
+    }
+]);
 
 export const signupButtonVariants = styleVariants({
     active: {
@@ -34,10 +34,6 @@ export const signupButtonVariants = styleVariants({
 });
 
 export const signupTextVariants = styleVariants({
-    active: {
-        color: vars.grayScale.grayscale00,
-    },
-    disabled: {
-        color: vars.grayScale.grayscale400,
-    },
+    active: [font.title20SB, { color: vars.grayScale.grayscale00 }],
+    disabled: [font.title20SB, { color: vars.grayScale.grayscale400 }],
 });
