cb5b209bfe333dd307823e55d0d99ca977e8788f
HAE2NI <hae0_02ni@ewhain.net>
2025-01-09 10:59:46 +0900

    [ design ] publishing

---
+++ angel_bridge/src/status/components/MissonCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function MissonCard() {
+  return (
+    <div>
+      <div>
+        <p>오늘의 미션</p>
+        <p>창업아이템 배경 및 필요성</p>
+        <p>
+          창업 아이템의 배경과 필요성을 명확히 하며 시작해봅시다! 시장과 소비자
+          문제를 정확히 정의함으로써 사업 모델(BM)을 구체화할 기초를 다지고,
+          창업 아이템이 제공할 수 있는 핵심 가치를 구상해보세요.
+        </p>
+      </div>
+      <button>미션 자세히 보기</button>
+    </div>
+  )
+}
