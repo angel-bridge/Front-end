60d9b8aa364b35c33b591c31d145189f499522fd
HAE2NI <hae0_02ni@ewhain.net>
2025-01-17 17:24:19 +0900

    [ etc ] delete codes

--- angel_bridge/src/app/status/page.tsx
+++ angel_bridge/src/app/status/page.tsx
@@ -1,11 +1,8 @@
-'use client'
 import UpperFrame from '@/status/components/UpperFrame'
-import React, { useState } from 'react'
+import React from 'react'
 import Header from '../home/components/Header'
 import { layout } from './pagelayout.css'
 import BottomFrame from '@/status/components/BottomFrame'
-import { vars } from '@/style/theme.css'
-import Modal from '@/status/components/modal/Modal'
 
 export default function page() {
   return (
