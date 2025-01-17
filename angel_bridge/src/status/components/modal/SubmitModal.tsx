e314c6e9dc9768dcbdbde714a4db82c64e21e859
HAE2NI <hae0_02ni@ewhain.net>
2025-01-17 20:17:23 +0900

    [ design ] 제출 모달달

--- angel_bridge/src/status/components/modal/SubmitModal.tsx
+++ angel_bridge/src/status/components/modal/SubmitModal.tsx
@@ -1,5 +1,57 @@
 import React from 'react'
+import { close_modal, gray_button_style } from '../../styles/button.css'
+import * as style from '../../styles/common.css'
+import modalcancel from '../../assets/modalCancel.svg'
+import Image from 'next/image'
+import { useModalStore } from '@/status/store/useModal'
+
+import * as input_style from '../../styles/input.css'
+import ErrorMessage from '@/mypage/memberInfo/components/ErrorMessage'
 
 export default function SubmitModal() {
-  return <div>SubmitModal</div>
+  const { setIsSubmitModalClose } = useModalStore()
+
+  function handleClickToSubmit() {
+    setIsSubmitModalClose()
+  }
+
+  return (
+    <div className={style.modal_background_two}>
+      <div className={style.modal_container}>
+        <div className={style.content_container}>
+          <div className={style.text_container_style}>
+            <div className={style.modal_title_closebtn}>
+              <p className={style.title_style}>2일차</p>
+              <div onClick={handleClickToSubmit} className={close_modal}>
+                <Image src={modalcancel} fill alt="모달 닫기" />
+              </div>
+            </div>
+
+            <div className={style.subText_container_style}>
+              <p className={style.second_title_style}>
+                창업아이템 배경 및 필요성
+              </p>
+              <p className={style.description_style}>
+                창업 아이템의 배경과 필요성을 명확히 하며 시작해봅시다! 시장과
+                소비자 문제를 정확히 정의함으로써 사업 모델(BM)을 구체화할
+                기초를 다지고, 창업 아이템이 제공할 수 있는 핵심 가치를
+                구상해보세요.
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className={input_style.input_label}>노션 링크 *</p>
+            <input
+              className={input_style.input_style}
+              placeholder="수행한 과제의 노션 링크를 제출해 주세요"
+            />
+            <ErrorMessage number={3} />
+            <button onClick={handleClickToSubmit} className={gray_button_style}>
+              미션 제출하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
