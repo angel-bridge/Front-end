a7c6d508619b2777fd39c7a5c216b3ebbac73dff
Gabin Lee <gb94@ewhain.net>
2025-01-18 16:24:15 +0900

    [ fix ] 카카오 로그인 서버 url 수정

--- angel_bridge/src/app/login/components/KakaoLogin.tsx
+++ angel_bridge/src/app/login/components/KakaoLogin.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function KakaoLogin() {
     return (
-        <a href="http://3.39.14.152/oauth2/authorization/kakao" target="_self">
+        <a href="https://api.angelbridge.site/oauth2/authorization/kakao" target="_self">
             <Image 
                 src='/assets/kakao_login.png'
                 alt='카카오 로그인 버튼'
