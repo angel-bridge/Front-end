d5857df7b00cba5760b4c1f21467b535df29fc00
HAE2NI <hae0_02ni@ewhain.net>
2025-01-20 16:42:07 +0900

    [ feat ] add authInstance

--- angel_bridge/src/api/authInstance.ts
+++ angel_bridge/src/api/authInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const authInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_URL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -9,10 +9,8 @@ export const authInstance = axios.create({
 
 authInstance.interceptors.request.use((config) => {
   const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN
-  //   const accessToken = Cookies.get('accessToken')
-  console.log('Authorization Header:', `Bearer ${accessToken}`)
   if (accessToken) {
-    config.headers['Authorization'] = accessToken
+    config.headers['Authorization'] = `Bearer ${accessToken}`
   }
   authInstance.interceptors.response.use(
     (response) => response.data,
