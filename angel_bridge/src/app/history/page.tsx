6566bc78f3f03c175e0493dc4b924dcdf2a0b248
Gabin Lee <gb94@ewhain.net>
2025-01-21 12:57:10 +0900

    [ fix ] 수강내역 페이지 header footer 삭제

--- angel_bridge/src/app/history/page.tsx
+++ angel_bridge/src/app/history/page.tsx
@@ -1,13 +1,9 @@
-import Header from '../home/components/Header';
 import Wrapper from './components/Wrapper';
-import Footer from '../home/components/Footer';
 
 export default function History() {
   return (
     <div>
-      <Header />
       <Wrapper />
-      <Footer />
     </div>
   );
 }
