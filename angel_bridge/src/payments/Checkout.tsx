e746cc72f90b4d5464985bc97ee8a1fe7620ad03
HAE2NI <hae0_02ni@ewhain.net>
2025-01-22 21:48:51 +0900

    [ feat ] 결제창 widget

--- angel_bridge/src/payments/Checkout.tsx
+++ angel_bridge/src/payments/Checkout.tsx
@@ -1,80 +1,114 @@
 'use client'
 import {
-  loadPaymentWidget,
-  PaymentWidgetInstance,
-} from '@tosspayments/payment-widget-sdk'
-import { ANONYMOUS } from '@tosspayments/tosspayments-sdk'
-import React, { useEffect, useRef, useState } from 'react'
-import { useAsync } from 'react-use'
+  ANONYMOUS,
+  loadTossPayments,
+  TossPaymentsWidgets,
+} from '@tosspayments/tosspayments-sdk'
+import { nanoid } from 'nanoid'
+import { useEffect, useState } from 'react'
+import * as style from './style.css'
+import usePostSaveAmount from './api/hooks/usePostSaveAmount'
 
-const clientKey = process.env.NEXT_PUBLIC_CLIENT_KEY as string
+export function CheckoutPage() {
+  const clientKey = process.env.NEXT_PUBLIC_CLIENT_KEY as string
+  const customerKey = 'UTuk_CpV40JbupUHAF0De'
 
-export default function Checkout() {
-  const paymentWidgetRef = useRef<PaymentWidgetInstance | null>(null)
-  const paymentMethodsWidgetRef = useRef<ReturnType<
-    PaymentWidgetInstance['renderPaymentMethods']
-  > | null>(null)
-  const [price, setPrice] = useState(50000)
+  const { mustate } = usePostSaveAmount()
 
-  useAsync(async () => {
-    const paymentWidget = await loadPaymentWidget(clientKey, ANONYMOUS)
-    const paymentMethodsWidget = paymentWidget.renderPaymentMethods(
-      '#payment-widget',
-      { value: price },
-      { variantKey: 'DEFAULT' },
-    )
-    paymentWidget.renderAgreement('#agreement', { variantKey: 'AGREEMENT' })
-    paymentWidgetRef.current = paymentWidget
-    paymentMethodsWidgetRef.current = paymentMethodsWidget
-  }, [])
+  const [amount, setAmount] = useState({
+    currency: 'KRW',
+    value: 50_000,
+  })
+  const [ready, setReady] = useState(false)
+  const [widgets, setWidgets] = useState<TossPaymentsWidgets | null>(null)
 
   useEffect(() => {
-    const paymentMethodsWidget = paymentMethodsWidgetRef.current
-    if (paymentMethodsWidget == null) {
+    async function fetchPaymentWidgets() {
+      // ------  결제위젯 초기화 ------
+      const tossPayments = await loadTossPayments(clientKey)
+      // 회원 결제
+      const widgets = tossPayments.widgets({
+        customerKey: ANONYMOUS,
+      })
+
+      setWidgets(widgets)
+    }
+
+    fetchPaymentWidgets()
+  }, [clientKey, customerKey])
+
+  useEffect(() => {
+    async function renderPaymentWidgets() {
+      if (widgets == null) {
+        return
+      }
+      // ------ 주문의 결제 금액 설정 ------
+      await widgets.setAmount(amount)
+
+      await Promise.all([
+        // ------  결제 UI 렌더링 ------
+        widgets.renderPaymentMethods({
+          selector: '#payment-method',
+          variantKey: 'DEFAULT',
+        }),
+        // ------  이용약관 UI 렌더링 ------
+        widgets.renderAgreement({
+          selector: '#agreement',
+          variantKey: 'AGREEMENT',
+        }),
+      ])
+
+      setReady(true)
+    }
+
+    renderPaymentWidgets()
+  }, [widgets])
+
+  useEffect(() => {
+    if (widgets == null) {
       return
     }
-    //새로운 결제 금액 넣어야 함요
-    paymentMethodsWidget.updateAmount(price)
-  }, [price])
+
+    widgets.setAmount(amount)
+  }, [widgets, amount])
+
   return (
-    <main
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
-      <h1>주문서</h1>
-      <span>{`${price.toLocaleString()}원`}</span>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            onChange={(event) => {
-              setPrice(event.target.checked ? price - 5000 : price + 5000)
-            }}
-          />
-          5,000원 할인 쿠폰 적용
-        </label>
+    <div className="wrapper">
+      <div className="box_section">
+        {/* 결제 UI */}
+        <div id="payment-method" />
+        {/* 이용약관 UI */}
+        <div id="agreement" />
+        {/* 쿠폰 체크박스 */}
+        <div></div>
+
+        {/* 결제하기 버튼 */}
+        <button
+          className={style.button_style}
+          disabled={!ready}
+          onClick={async () => {
+            try {
+              // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
+              // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+              // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
+              await widgets.requestPayment({
+                orderId: 'yqPwvWha_N1asJ5DC1l9J',
+                orderName: '토스 티셔츠 외 2건',
+                successUrl: window.location.origin + '/success',
+                failUrl: window.location.origin + '/fail',
+                customerEmail: 'customer123@gmail.com',
+                customerName: '김토스',
+                customerMobilePhone: '01012341234',
+              })
+            } catch (error) {
+              // 에러 처리하기
+              console.error(error)
+            }
+          }}
+        >
+          결제하기
+        </button>
       </div>
-      <div id="payment-widget" style={{ width: '100%' }} />
-      <div id="agreement" style={{ width: '100%' }} />
-      <button
-        onClick={async () => {
-          const paymentWidget = paymentWidgetRef.current
-          try {
-            await paymentWidget?.requestPayment({
-              orderId: 'ujRprh3uWK8Wt-UHEX82B',
-              orderName: '토스 티셔츠 외 2건',
-              customerName: '김토스',
-              customerEmail: 'customer123@gmail.com',
-              customerMobilePhone: '01012341234',
-              // successUrl: `${window.location.origin}/success`,
-              // failUrl: `${window.location.origin}/fail`,
-            })
-          } catch (error) {
-            console.error(error)
-          }
-        }}
-      >
-        결제하기
-      </button>
-    </main>
+    </div>
   )
 }
