de3affea080209e2f02ff8743fcaeb07e0ead28d
HAE2NI <hae0_02ni@ewhain.net>
2025-01-23 14:30:36 +0900

    [ feat ] add educationId params

--- angel_bridge/src/payments/SuccessPage.tsx
+++ angel_bridge/src/payments/SuccessPage.tsx
@@ -10,8 +10,13 @@ export default function SuccessPage() {
   const paymentKey = searchParams.get('paymentKey')
   const orderId = searchParams.get('orderId')
   const amount = searchParams.get('amount')
+  const educationId = searchParams.get('educationId')
 
-  async function confirmPayment({ educationId }: { educationId: number }) {
+  async function confirmPayment({
+    educationId,
+  }: {
+    educationId: string | null
+  }) {
     // TODO: API를 호출해서 서버에게 paymentKey, orderId, amount를 넘겨주세요.
     // 서버에선 해당 데이터를 가지고 승인 API를 호출하면 결제가 완료됩니다.
     // https://docs.tosspayments.com/reference#%EA%B2%B0%EC%A0%9C-%EC%8A%B9%EC%9D%B8
