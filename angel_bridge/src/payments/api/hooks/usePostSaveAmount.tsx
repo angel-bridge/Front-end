d994dc084aa5bfb029206c4cc0ba6df6dbf4384a
HAE2NI <hae0_02ni@ewhain.net>
2025-01-23 14:30:21 +0900

    [ type ] change type of amount

--- angel_bridge/src/payments/api/hooks/usePostSaveAmount.tsx
+++ angel_bridge/src/payments/api/hooks/usePostSaveAmount.tsx
@@ -3,11 +3,21 @@
 import { useMutation } from '@tanstack/react-query'
 import { postSaveAmount } from '../utils/postSaveAmount'
 
+export interface AmountType {
+  currency: string
+  value: number
+}
+
 export default function usePostSaveAmount() {
   const { mutate } = useMutation({
     mutationKey: ['postSaveAmount'],
-    mutationFn: ({ orderId, amount }: { orderId: string; amount: number }) =>
-      postSaveAmount(orderId, amount),
+    mutationFn: ({
+      orderId,
+      amount,
+    }: {
+      orderId: string
+      amount: AmountType
+    }) => postSaveAmount(orderId, amount),
   })
   return { mutate }
 }
