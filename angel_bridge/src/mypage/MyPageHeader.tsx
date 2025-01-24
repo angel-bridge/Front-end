0ccfcc6be75ef6a62f046c370f55c022b34c4fbc
HAE2NI <hae0_02ni@ewhain.net>
2025-01-24 23:09:07 +0900

    [ feat ] prefetch 추가가

--- angel_bridge/src/mypage/MyPageHeader.tsx
+++ angel_bridge/src/mypage/MyPageHeader.tsx
@@ -3,18 +3,25 @@ import { button_container } from './memberInfo/styles/button.css'
 import InfoBtn from './common/components/InfoBtn'
 import IsPaidBtn from './common/components/IsPaidBtn'
 import { usePathname, useRouter } from 'next/navigation'
+import { useQueryClient } from '@tanstack/react-query'
+import { getPayments } from './api/utils/getPayments'
 
 export default function MyPageHeader() {
+  const queryClient = useQueryClient()
   const router = useRouter()
   const pathname = usePathname()
-  const isInfo = !pathname.includes('payment')
+  const isInfo = !pathname.includes('myPurchasedList')
 
   function handleSwitchToInfo() {
     router.push('/mypage')
   }
 
   function handleSwitchToPaied() {
-    router.push('/mypage/payment')
+    queryClient.prefetchQuery({
+      queryKey: ['getPayments'],
+      queryFn: getPayments,
+    })
+    router.push('/myPurchasedList')
   }
 
   return (
