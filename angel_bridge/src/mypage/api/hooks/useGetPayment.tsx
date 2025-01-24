783848a2b003f6bdd32022084b17153f7d3f91b3
HAE2NI <hae0_02ni@ewhain.net>
2025-01-24 22:47:53 +0900

    [ feat ] add initalData

--- angel_bridge/src/mypage/api/hooks/useGetPayment.tsx
+++ angel_bridge/src/mypage/api/hooks/useGetPayment.tsx
@@ -1,11 +1,15 @@
 'use client'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { getPayments } from '../utils/getPayments'
 
 export default function useGetPayment() {
+  const queryClient = useQueryClient()
   const { data, isLoading, isError } = useQuery({
     queryKey: ['getPayments'],
     queryFn: () => getPayments(),
+
+    staleTime: 5 * 60 * 1000,
+    initialData: queryClient.getQueryData(['getPayments']),
   })
 
   return { data, isLoading, isError }
