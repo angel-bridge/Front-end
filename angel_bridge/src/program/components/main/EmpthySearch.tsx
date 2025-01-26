74547cc3d446612c9e83601fe9d0138cc2bd4b3a
HAE2NI <hae0_02ni@ewhain.net>
2025-01-26 17:19:27 +0900

    [ desing ] add empthy view

---
+++ angel_bridge/src/program/components/main/EmpthySearch.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+import searchIcon from '../../assets/searchIc.svg'
+import { empthy_icon, empthy_view_container } from '@/program/styles/empthyview'
+
+export default function EmptySearch() {
+  return (
+    <div className={empthy_view_container}>
+      <div className={empthy_icon}>
+        <Image src={searchIcon} fill alt="검색 아이콘" />
+      </div>
+      <p>앗! 검색 결과가 없어요</p>
+    </div>
+  )
+}
---
+++ angel_bridge/src/program/components/main/EmpthyView.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+import preparingIc from '../../assets/preparingIc.svg'
+import { empthy_icon, empthy_view_container } from '@/program/styles/empthyview'
+
+export default function EmpthyView() {
+  return (
+    <div className={empthy_view_container}>
+      <div className={empthy_icon}>
+        <Image src={preparingIc} fill alt="준비중" />
+      </div>
+      <p>앗! 아직 준비중이에요</p>
+    </div>
+  )
+}
