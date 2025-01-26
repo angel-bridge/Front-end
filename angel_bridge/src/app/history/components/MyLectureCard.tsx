8ad5e570c3ef22ddc97d697e407a38025a555198
HAE2NI <hae0_02ni@ewhain.net>
2025-01-26 17:06:23 +0900

    [ feat ] add Link tag

--- angel_bridge/src/app/history/components/MyLectureCard.tsx
+++ angel_bridge/src/app/history/components/MyLectureCard.tsx
@@ -1,40 +1,54 @@
-import Image from 'next/image';
-import Button from '@/components/common/Button';
-import * as style from '../styles/MyLectureCard.css';
+import Image from 'next/image'
+import Button from '@/components/common/Button'
+import * as style from '../styles/MyLectureCard.css'
+import Link from 'next/link'
 
 interface CardProps {
-    thumbnail: string; // 썸네일 이미지
-    state: string; // 수강중, 수강예정, 수강 완료
-    start: string;
-    end: string;
-    title: string;
-    bio: string;
+  thumbnail: string // 썸네일 이미지
+  state: string // 수강중, 수강예정, 수강 완료
+  start: string
+  end: string
+  title: string
+  bio: string
+  educationId: number
 }
 
-export default function MyLectureCard({ thumbnail, state, start, end, title, bio }: CardProps) {
-    const isValid = state === '수강중' || state === '수강완료';
+export default function MyLectureCard({
+  thumbnail,
+  state,
+  start,
+  end,
+  title,
+  bio,
+  educationId,
+}: CardProps) {
+  const isValid = state === '수강중' || state === '수강완료'
 
-    return (
-        <div className={style.contents}>
-            <div className={style.contentInfo}>
-                <div className={style.contentImage}>
-                    <Image
-                        src={thumbnail}
-                        alt="Program Thumbnail"
-                        fill
-                        style={{
-                            objectFit: 'cover',
-                            borderRadius: '24px',
-                        }}
-                    />
-                </div>
-                <div className={style.contentText}>
-                    <div className={style.contentPeriod}>{start} - {end}</div>
-                    <div className={style.contentTitle}>{title}</div>
-                    <div className={style.contentBio}>{bio}</div>
-                </div>
-            </div>
-            <Button isValid={isValid} text='미션 바로가기'/>
+  return (
+    <div className={style.contents}>
+      <div className={style.contentInfo}>
+        <div className={style.contentImage}>
+          <Image
+            src={thumbnail}
+            alt="Program Thumbnail"
+            fill
+            style={{
+              objectFit: 'cover',
+              borderRadius: '24px',
+            }}
+          />
         </div>
-    );
+        <div className={style.contentText}>
+          <div className={style.contentPeriod}>
+            {start} - {end}
+          </div>
+          <div className={style.contentTitle}>{title}</div>
+          <div className={style.contentBio}>{bio}</div>
+        </div>
+      </div>
+      <Link href={`/status/${educationId}`}>
+        <Button isValid={isValid} text="미션 바로가기" />
+      </Link>
+    </div>
+  )
 }
