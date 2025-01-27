512cba7cb608743ae8322486e6015ba2e65e3e2f
HAE2NI <hae0_02ni@ewhain.net>
2025-01-27 21:18:51 +0900

    [ fix ] fix import error

--- angel_bridge/src/api/blog.tsx
+++ angel_bridge/src/api/blog.tsx
@@ -1,21 +1,21 @@
-import { instance } from "./authInstance";
+import { instance } from './instance'
 
 export interface BlogResult {
-    title: string;
-    content: string;
-    postingDate: string;
-    link: string;
+  title: string
+  content: string
+  postingDate: string
+  link: string
 }
 
 interface FetchBlogResponse {
-    timestamp: string;
-    code: number;
-    message: string;
-    result: BlogResult[];
+  timestamp: string
+  code: number
+  message: string
+  result: BlogResult[]
 }
 
 export const getBlogArticles = async () => {
-    const response: FetchBlogResponse = await instance.get(`/api/v1/blog/latest`);
-    // console.log('블로그 아티클 가져옴!!', response);
-    return response.result;
-};
+  const response: FetchBlogResponse = await instance.get(`/api/v1/blog/latest`)
+  // console.log('블로그 아티클 가져옴!!', response);
+  return response.result
+}
