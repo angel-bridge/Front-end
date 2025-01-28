fe8ab8b5548c40882590aaabfa92846c70a5186c
HAE2NI <hae0_02ni@ewhain.net>
2025-01-28 20:00:06 +0900

    [ feat ] add data

--- angel_bridge/src/status/components/UpperFrame.tsx
+++ angel_bridge/src/status/components/UpperFrame.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import StatusCard from './StatusCard'
 import MissonCard from './MissonCard'
@@ -7,12 +8,18 @@ import {
   upper_content_container,
   upper_title_style,
 } from '../styles/common.css'
+import useGetMissionBox from '../api/hooks/useGetMissionBox'
+import { useParams } from 'next/navigation'
 
 export default function UpperFrame() {
+  const params = useParams()
+  const { educationId } = params
+  const { data } = useGetMissionBox(educationId)
+
   return (
     <div className={upper_container}>
       <div className={upper_content_container}>
-        <p className={upper_title_style}>예비창업패키지 2주 완성</p>
+        <p className={upper_title_style}>{data?.educationTitle} </p>
         <div className={upper_cards_container}>
           <MissonCard />
           <StatusCard />
