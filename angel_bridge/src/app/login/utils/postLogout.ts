80bbdc49ca74cafd96902a051f66f1f5c0fbb348
Gabin Lee <gb94@ewhain.net>
2025-01-28 12:13:28 +0900

    [ fix ] postLogout withCredentials 옵션 추가

--- angel_bridge/src/app/login/utils/postLogout.ts
+++ angel_bridge/src/app/login/utils/postLogout.ts
@@ -2,7 +2,7 @@ import { authInstance } from '@/api/authInstance';
 
 export const postLogout = async () => {
     try {
-        const { data } = await authInstance.post('/logout');
+        const { data } = await authInstance.post('/logout', { withCredentials: true });
         console.log('Logout Response:', data);
         return data;
     } catch (error) {
