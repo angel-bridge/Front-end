c618966edf0d8a92a0621e6fd677c82b6bb5b35e
HAE2NI <hae0_02ni@ewhain.net>
2025-01-28 21:12:36 +0900

    [ feat ] add data

--- angel_bridge/src/status/components/UpperFrame.tsx
+++ angel_bridge/src/status/components/UpperFrame.tsx
@@ -14,15 +14,29 @@ import { useParams } from 'next/navigation'
 export default function UpperFrame() {
   const params = useParams()
   const { educationId } = params
-  const { data } = useGetMissionBox(educationId)
+  const { data, isLoading } = useGetMissionBox(educationId)
+
+  console.log(data)
+
+  if (isLoading) {
+    return <p>loading...</p>
+  }
 
   return (
     <div className={upper_container}>
       <div className={upper_content_container}>
         <p className={upper_title_style}>{data?.educationTitle} </p>
         <div className={upper_cards_container}>
-          <MissonCard />
-          <StatusCard />
+          <MissonCard
+            assignmentId={data?.assignmentId}
+            assignmentTitle={data?.assignmentTitle}
+            description={data?.description}
+          />
+          <StatusCard
+            performanceRate={data?.performanceRate}
+            noticeLink={data?.noticeLink}
+            methodLink={data?.methodLink}
+          />
         </div>
       </div>
     </div>
