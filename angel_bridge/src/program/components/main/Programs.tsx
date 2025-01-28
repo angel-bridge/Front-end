2c6bfb2081f9480650d8b7996082aec3910f0ff4
Gabin Lee <gb94@ewhain.net>
2025-01-28 13:12:05 +0900

    [ design ] 프로그램 카드 링크 언더라인 없애기

--- angel_bridge/src/program/components/main/Programs.tsx
+++ angel_bridge/src/program/components/main/Programs.tsx
@@ -15,7 +15,7 @@ export default function Programs({
         const { description, recruitmentStatus, title, educationId, preImage } =
           data
         return (
-          <Link href={`program/${educationId}`} key={educationId}>
+          <Link style={{ textDecoration: 'none' }} href={`program/${educationId}`} key={educationId}>
             <div key={educationId}>
               <ProgramCard
                 badgeText={recruitmentStatus}
