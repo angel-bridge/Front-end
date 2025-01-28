c76a0ff8270e983a7e7d03c5be307faeec3aaa01
HAE2NI <hae0_02ni@ewhain.net>
2025-01-28 23:37:54 +0900

    [ etc ] delete code

--- angel_bridge/src/program/detailPage/components/BannerAndPurchase.tsx
+++ angel_bridge/src/program/detailPage/components/BannerAndPurchase.tsx
@@ -1,35 +1,12 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Banner from './Banner'
 import PurchaseBtn from './PurchaseBtn'
-import { wrapper } from '../styles/purchase.css'
 
 export default function BannerAndPurchase() {
-  const [isFixed, setIsFixed] = useState(false)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const wrapperElement = document.querySelector(`.${wrapper}`)!
-      const rect = wrapperElement.getBoundingClientRect()
-
-      if (rect.top <= 0) {
-        setIsFixed(true)
-      } else {
-        setIsFixed(false)
-      }
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
-
   return (
     <div>
       <Banner />
-      <div className={wrapper}>
-        <PurchaseBtn isFixed={isFixed} />
-      </div>
+      <PurchaseBtn />
     </div>
   )
 }
