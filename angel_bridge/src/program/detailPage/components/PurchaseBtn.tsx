7bfc761aacbd97cbe5e039b9a52cffddb71d34b0
HAE2NI <hae0_02ni@ewhain.net>
2025-01-28 23:23:55 +0900

    [ feat ] add useEffect to fixed

--- angel_bridge/src/program/detailPage/components/PurchaseBtn.tsx
+++ angel_bridge/src/program/detailPage/components/PurchaseBtn.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   button_style,
   detail,
@@ -8,15 +8,34 @@ import {
   purchased_sticky,
   text,
   title,
+  wrapper,
 } from '../styles/purchase.css'
 import Image from 'next/image'
 import purchaseIcon from '../assets/purchase.svg'
 import { useParams, useRouter } from 'next/navigation'
 
-export default function PurchaseBtn({ isFixed }: { isFixed: boolean }) {
+export default function PurchaseBtn() {
   const params = useParams()
   const router = useRouter()
   const educationId = params.educationId as string
+  const [isFixed, setIsFixed] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const wrapperElement = document.querySelector(`.${wrapper}`)!
+      const rect = wrapperElement.getBoundingClientRect()
+
+      if (rect.top <= 0) {
+        setIsFixed(true)
+      } else {
+        setIsFixed(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  })
 
   function onClickPurchaseBtn() {
     router.push(`/payments?eucationId=${educationId}`)
@@ -26,18 +45,20 @@ export default function PurchaseBtn({ isFixed }: { isFixed: boolean }) {
   }
 
   return (
-    <div className={isFixed ? purchased_fixed : purchased_sticky}>
-      <div className={text}>
-        <p className={title}>예비창업패키지 2주 완성</p>
-        <p className={detail}>12월 15일 (일) 자정 마감</p>
-      </div>
+    <div className={wrapper}>
+      <div className={isFixed ? purchased_fixed : purchased_sticky}>
+        <div className={text}>
+          <p className={title}>예비창업패키지 2주 완성</p>
+          <p className={detail}>12월 15일 (일) 자정 마감</p>
+        </div>
 
-      <button onClick={onClickPurchaseBtn} className={button_style}>
-        <span className={icon}>
-          <Image src={purchaseIcon} alt="구매" fill />
-        </span>
-        구매하기
-      </button>
+        <button onClick={onClickPurchaseBtn} className={button_style}>
+          <span className={icon}>
+            <Image src={purchaseIcon} alt="구매" fill />
+          </span>
+          구매하기
+        </button>
+      </div>
     </div>
   )
 }
