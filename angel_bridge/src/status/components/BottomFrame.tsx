f9bd9a71e340a2ae0017615823ed3e9959c6969e
HAE2NI <hae0_02ni@ewhain.net>
2025-01-28 19:55:27 +0900

    [ feat ] add function to switch page

--- angel_bridge/src/status/components/BottomFrame.tsx
+++ angel_bridge/src/status/components/BottomFrame.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import * as style from '../styles/bottom.css'
 import { DUMMY_DATA } from '../core/submitStatus'
 import MissionSubmitStatusCard from './MissionSubmitStatusCard'
 import { bottom_black_style, bottom_gray_style } from '../styles/button.css'
+import useGetMissionStatusList from '../api/hooks/useGetMissionStatusList'
+import { useParams } from 'next/navigation'
 
 export default function BottomFrame() {
+  const params = useParams()
+  const educationId = params.educationId as string
+  const [page, setPage] = useState(1)
+
+  const { data } = useGetMissionStatusList(educationId, page)
+
+  console.log(data)
+
+  function handleSetPage(number: number) {
+    setPage(number)
+  }
+
   return (
     <div className={style.bottom_contaeinr}>
       <p className={style.title_style}>미션 제출 현황</p>
       <div className={style.buttons_container}>
-        <button className={bottom_black_style}>1-7회차</button>
-        <button className={bottom_gray_style}>8-14회차</button>
-        <button className={bottom_gray_style}>15-21회차</button>
+        <button onClick={() => handleSetPage(1)} className={bottom_black_style}>
+          1-7회차
+        </button>
+        <button onClick={() => handleSetPage(2)} className={bottom_gray_style}>
+          8-14회차
+        </button>
+        <button onClick={() => handleSetPage(3)} className={bottom_gray_style}>
+          15-21회차
+        </button>
       </div>
       <div className={style.submitCards_container}>
         {DUMMY_DATA.map((data) => {
