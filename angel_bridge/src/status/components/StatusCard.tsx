b4ad06894f2bc75b4ce3ab6ae53f6fe3a5e7b295
HAE2NI <hae0_02ni@ewhain.net>
2025-01-28 21:12:17 +0900

    [ feat ] add Link tag to go to another Link

--- angel_bridge/src/status/components/StatusCard.tsx
+++ angel_bridge/src/status/components/StatusCard.tsx
@@ -2,18 +2,33 @@ import React from 'react'
 import PieChartCard from './PieChartCard'
 import { NOTION_LIST } from '../core/notion'
 import Notion from './Notion'
-import { title } from 'process'
 import { notions_container, status_constainer } from '../styles/status.css'
+import Link from 'next/link'
 
 //미션 수행도 카드드 + 파이 차트
-export default function StatusCard() {
+export default function StatusCard({
+  noticeLink,
+  methodLink,
+  performanceRate,
+}: {
+  noticeLink: string | undefined
+  methodLink: string | undefined
+  performanceRate: number | undefined
+}) {
   return (
     <div className={status_constainer}>
-      <PieChartCard />
+      <PieChartCard performanceRate={performanceRate} />
       <div className={notions_container}>
-        {NOTION_LIST.map((data) => {
-          return <Notion key={title} title={data.title} img={data.img} />
-        })}
+        {noticeLink && (
+          <Link href={noticeLink && noticeLink}>
+            <Notion title={NOTION_LIST[0].title} img={NOTION_LIST[0].img} />
+          </Link>
+        )}
+        {methodLink && (
+          <Link href={methodLink}>
+            <Notion title={NOTION_LIST[1].title} img={NOTION_LIST[1].img} />
+          </Link>
+        )}
       </div>
     </div>
   )
