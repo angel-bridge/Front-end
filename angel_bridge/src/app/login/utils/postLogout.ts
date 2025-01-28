162e950a832bafae32d1217728bf8ac30ea1a1a6
Gabin Lee <gb94@ewhain.net>
2025-01-28 12:31:03 +0900

    [ fix ] 두 번째 인자 empty body

--- angel_bridge/src/app/login/utils/postLogout.ts
+++ angel_bridge/src/app/login/utils/postLogout.ts
@@ -2,7 +2,10 @@ import { authInstance } from '@/api/authInstance';
 
 export const postLogout = async () => {
     try {
-        const { data } = await authInstance.post('/logout', { withCredentials: true });
+        const { data } = await authInstance.post(
+            '/logout', 
+            {},
+            { withCredentials: true });
         console.log('Logout Response:', data);
         return data;
     } catch (error) {
