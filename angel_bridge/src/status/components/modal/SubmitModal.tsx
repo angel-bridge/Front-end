1bb6e9ef624d54b68301ad6d471fabe37c0c0a07
HAE2NI <hae0_02ni@ewhain.net>
2025-01-29 10:59:54 +0900

    [ feat ] add mutate function to handle Submit mission

--- angel_bridge/src/status/components/modal/SubmitModal.tsx
+++ angel_bridge/src/status/components/modal/SubmitModal.tsx
@@ -13,10 +13,20 @@ import { useModalStore } from '@/status/store/useModal'
 import * as input_style from '../../styles/input.css'
 import ErrorMessage from '@/mypage/memberInfo/components/ErrorMessage'
 import { content_container } from '@/status/styles/modal.css'
+import { useSubmit } from '@/status/store/useSubmit'
+import usePatchMissionLink from '@/status/api/hooks/usePatchMissionLink'
+import { useParams } from 'next/navigation'
 
+//미션제출 모달입니다
 export default function SubmitModal() {
+  const { title, description, assignmentId } = useSubmit()
   const [notionLink, setNotionLink] = useState('')
   const [isError, setIsError] = useState(false)
+
+  const params = useParams()
+  const educationId = params.educationId as string
+
+  const { mutate } = usePatchMissionLink()
 
   const { setIsSubmitModalClose } = useModalStore()
   const urlRegex = /^(https?|ftp):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i
@@ -32,6 +42,7 @@ export default function SubmitModal() {
 
   function handleClickToSubmit() {
     setIsSubmitModalClose()
+    mutate({ educationId, assignmentId, submissionLink: notionLink })
   }
 
   return (
@@ -40,22 +51,15 @@ export default function SubmitModal() {
         <div className={content_container}>
           <div className={style.text_container_style}>
             <div className={style.modal_title_closebtn}>
-              <p className={style.title_style}>2일차</p>
+              <p className={style.title_style}>{assignmentId}일차</p>
               <div onClick={handleClickToSubmit} className={close_modal}>
                 <Image src={modalcancel} fill alt="모달 닫기" />
               </div>
             </div>
 
             <div className={style.subText_container_style}>
-              <p className={style.second_title_style}>
-                창업아이템 배경 및 필요성
-              </p>
-              <p className={style.description_style}>
-                창업 아이템의 배경과 필요성을 명확히 하며 시작해봅시다! 시장과
-                소비자 문제를 정확히 정의함으로써 사업 모델(BM)을 구체화할
-                기초를 다지고, 창업 아이템이 제공할 수 있는 핵심 가치를
-                구상해보세요.
-              </p>
+              <p className={style.second_title_style}>{title}</p>
+              <p className={style.description_style}>{description}</p>
             </div>
           </div>
 
