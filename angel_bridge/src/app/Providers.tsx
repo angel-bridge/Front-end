abcea58b3872cecae90212a41d65c1c37329e1e8
HAE2NI <hae0_02ni@ewhain.net>
2025-01-29 18:58:28 +0900

    [ feat ] fix provider

--- angel_bridge/src/app/Providers.tsx
+++ angel_bridge/src/app/Providers.tsx
@@ -1,14 +1,18 @@
-"use client";
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+'use client'
+import { Suspense, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
+import Loading from './Loading'
 export default function Providers({ children }: React.PropsWithChildren) {
-  const [client] = useState(new QueryClient());
+  const [client] = useState(new QueryClient())
+
   return (
-    <QueryClientProvider client={client}>
-      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+    <Suspense fallback={<Loading />}>
+      <QueryClientProvider client={client}>
+        <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Suspense>
+  )
 }
