dcbd516ef61a3a54ab74ad00baad3212052adebb
HAE2NI <hae0_02ni@ewhain.net>
2025-01-29 18:45:00 +0900

    [ feat ] loading component

---
+++ angel_bridge/src/app/Loading.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+import loading from './loading.gif'
+
+export default function Loading() {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: '10',
+        height: '100%',
+        backgroundColor: ' #FFF',
+        width: '100%',
+      }}
+    >
+      <div style={{ position: 'relative', width: '12rem', height: '12rem' }}>
+        <Image src={loading} alt="로딩뷰" fill />
+      </div>
+    </div>
+  )
+}
